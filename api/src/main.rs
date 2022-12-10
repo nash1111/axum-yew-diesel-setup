97180869d5137d7a0339a45ac83d0f184e23e89a
nash1111 <ken1251213@gmail.com>
2022-12-10 11:51:14 +0900

    Add non-docker frontend

--- api/src/main.rs
+++ api/src/main.rs
@@ -8,7 +8,7 @@ async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
