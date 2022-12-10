63105361f43f5f951533a6e5bc90bace221618a1
nash1111 <ken1251213@gmail.com>
2022-12-10 10:18:20 +0900

    Add axum api

---
+++ api/src/main.rs
@@ -0,0 +1,15 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
