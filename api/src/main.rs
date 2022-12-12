793366e3d05607f0c40d61026e14e8db08138c19
nash1111 <ken1251213@gmail.com>
2022-12-12 13:24:39 +0900

    First test

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,14 +1,7 @@
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json,
-    Router,
-};
-use serde::{Deserialize, Serialize};
+use axum::{ http::StatusCode, response::IntoResponse, routing::{ get, post }, Json, Router };
+use serde::{ Deserialize, Serialize };
 use std::env;
 use std::net::SocketAddr;
-
 
 #[tokio::main]
 async fn main() {
@@ -18,33 +11,55 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }))
-        .route("/users", post(create_user));
+    let app = create_app();
 
     let addr = &"0.0.0.0:8000".parse().unwrap();
-    axum::Server::bind(addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    axum::Server::bind(addr).serve(app.into_make_service()).await.unwrap();
     tracing::debug!("listening on {}", addr);
 }
 
-async fn create_user (Json(payload): Json<CreateUser>)-> impl IntoResponse {
+fn create_app() -> Router {
+    Router::new()
+        .route(
+            "/",
+            get(|| async { "Hello, World!" })
+        )
+        .route("/users", post(create_user))
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
     let user = User {
         id: 1337,
         username: payload.username,
     };
     (StatusCode::CREATED, Json(user))
-
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 struct CreateUser {
     username: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 struct User {
     id: u64,
     username: String,
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{ header, Method, Request},
+    };
+    use tower::ServiceExt;
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello, World!");
+    }
+    }
